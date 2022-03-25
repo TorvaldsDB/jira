ce3efda5452ebeea4dbc1d5477635d6562c14730
bdu <bdu@appen.com>
2022-03-25 21:41:46 +0800

    feat: add project list

--- src/screens/ProjectList/List.jsx
+++ src/screens/ProjectList/List.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export const List = () => {
+export const List = ({ list, users }) => {
   return (
-    <table>List</table>
-  )
-}
+    <table>
+      <thead>
+        <tr>
+          <th>名称</th>
+          <th>负责人</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map((project) => (
+          <tr key={project.id}>
+            <td>{project.name}</td>
+            <td>
+              {users.find((user) => user.id === project.personId)?.name ||
+                "未知"}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
--- src/screens/ProjectList/index.jsx
+++ src/screens/ProjectList/index.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import { List } from './List'
-import { SearchPanel } from './SearchPanel'
+import React, { useState, useEffect } from "react";
+import { cleanObject } from "../../utils";
+import { List } from "./List";
+import { SearchPanel } from "./SearchPanel";
+import * as qs from "qs";
+
+const apiURL = process.env.REACT_APP_API_URL;
 
 export const ProjectList = () => {
+  const [users, setUsers] = useState([]);
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${apiURL}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json());
+        }
+      }
+    );
+  }, [param]);
+
+  useEffect(() => {
+    fetch(`${apiURL}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+
   return (
-    <div>  
-        <SearchPanel></SearchPanel>
-        <List></List>
+    <div>
+      <SearchPanel
+        users={users}
+        param={param}
+        setParam={setParam}
+      ></SearchPanel>
+      <List users={users} list={list}></List>
     </div>
-  )
-}
+  );
+};
