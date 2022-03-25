d3e3d614dd4f7d9b45cf984a0a031ec038b75103
bdu <bdu@appen.com>
2022-03-26 01:15:07 +0800

    chore: any -> unknow

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
+// export const isFalsy: (value: any) => boolean = (value) => value === 0 ? false : !value;
+
 export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(object).forEach((key) => {
@@ -22,7 +24,7 @@ export const useMount = (callback: () => void) => {
   }, []);
 };
 
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = (value: unknown, delay?: number): any => {
   const [debounceValue, setDebounceValue] = useState(value);
   useEffect(() => {
     const timeout = setTimeout(() => setDebounceValue(value), delay);
