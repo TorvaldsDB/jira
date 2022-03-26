fe66edcafbc46827b7d3cdd7cbdf73f19c3fbcce
bdu <bdu@appen.com>
2022-03-26 11:30:17 +0800

    chore: test TS

---
+++ src/TsReactTest.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { useMount } from "utils";
+
+type Props = {};
+
+const TsReactTest = (props: Props) => {
+  const persons: { name: string; age: number }[] = [
+    { name: "jack", age: 25 },
+    { name: "maya", age: 22 },
+  ];
+  const { value, clear, removeIndex, add } = useArray(persons);
+  useMount(() => {});
+  return (
+    <div>
+      <button onClick={() => add({ name: "john", age: 22 })}>add john</button>
+      <button onClick={() => removeIndex(0)}>remove 0</button>
+      <button style={{ marginBottom: "50px" }} onClick={() => clear()}>
+        clear
+      </button>
+      {value.map((person: { name: string; age: number }, index: number) => (
+        <div style={{ color: "red" }}>
+          <span style={{ color: "red" }}>{index}</span>
+          <span>{person.name}</span>
+          <span>{person.age}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TsReactTest;
+
+const useArray = <T,>(initialValue: T[]) => {
+  const [value, setValue] = useState(initialValue);
+  return {
+    value,
+    setValue,
+    add: (item: T): void => {
+      setValue([...value, item]);
+    },
+    removeIndex: (index: number): void => {
+      const copy = [...value];
+      copy.splice(index, 0);
+      setValue(copy);
+    },
+    clear: () => {
+      setValue([]);
+    },
+  };
+};
