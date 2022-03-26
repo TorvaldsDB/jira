fc3749b34084d6e85d9d1f48cd0c05f4180d09f3
bdu <bdu@appen.com>
2022-03-26 20:31:16 +0800

    feat: auth context

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,21 +1,10 @@
+import { useAuth } from "context/auth-context";
 import React from "react";
 
 const apiURL = process.env.REACT_APP_API_URL;
 
 const Login = () => {
-  const login = (param: { username: string; password: string }) => {
-    fetch(`${apiURL}/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(param),
-    }).then(async (response) => {
-      if (response.ok) {
-        console.log("login successfully");
-      }
-    });
-  };
+  const { login, user } = useAuth();
   // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
   //   event.preventDefault();
   //   const username = (event.currentTarget.elements[0] as HTMLInputElement)
@@ -39,6 +28,12 @@ const Login = () => {
 
   return (
     <form onSubmit={handleSubmit}>
+      {user ? (
+        <div>
+          登陆成功, 用户名: {user?.name} token: {user.token}
+        </div>
+      ) : null}
+
       <div>
         <label htmlFor="username">用户名</label>
         <input type="text" id="username" />
