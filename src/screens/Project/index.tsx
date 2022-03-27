d2c007bbabac3cfb6444112949aab42bd5771c35
bdu <bdu@appen.com>
2022-03-27 21:27:06 +0800

    feat: kanban 与 epic 路由

--- src/screens/Project/index.tsx
+++ src/screens/Project/index.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import Kanban from "screens/Kanban";
+import Epic from "screens/Epic";
 
-function Project() {
-  return <div>Project</div>;
-}
+const Project = () => {
+  return (
+    <div>
+      <h1>Project</h1>
+      <Link to="kanban">看板</Link>
+      <Link to="epic">任务组</Link>
+
+      <Routes>
+        <Route path="/kanban" element={<Kanban />}></Route>
+        <Route path="/epic" element={<Epic />}></Route>
+        <Route index element={<Kanban />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default Project;
